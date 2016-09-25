e154229669450d831f4abd3ae065d2eb37f3a7eb
dsanchez <dsanch17@uncc.edu>
2016-09-25 00:03:08 -0400

    redid Async_timer to be more accurate

--- app/src/main/java/edu/itcs4180/hw4_triviaapp/Async_Timer.java
+++ app/src/main/java/edu/itcs4180/hw4_triviaapp/Async_Timer.java
@@ -8,31 +8,53 @@ package edu.itcs4180.hw4_triviaapp;
  */
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.util.GregorianCalendar;
 
 
 public class Async_Timer extends AsyncTask<Void, Integer, Void> {
 
-    int timeLeft;
     int totalTime;
     TriviaTimer triviaTimer;
 
     Async_Timer(TriviaTimer triviaTimer, int totalTime) {
         this.totalTime = totalTime;
-        this.timeLeft = totalTime;
         this.triviaTimer = triviaTimer;
     }
 
 
     @Override
     protected Void doInBackground(Void... voids) {
-        while (timeLeft > 0) {
-            publishProgress(timeLeft);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-               // e.printStackTrace();
+
+        //initialize counter can and find initial time
+        int timeLeft = totalTime;
+        int counter = 0;
+        int initTime = (int) (new GregorianCalendar().getTimeInMillis() / 1000);
+        Log.d("testTime","initTime: " + initTime);
+
+
+        //while the timeLeft value is greater than 0 execute as long as not canceled
+        while (timeLeft > 0 && !this.isCancelled()) {
+
+            //get the currentTime in seconds at the top of the loop
+            int timeNow = (int) (new GregorianCalendar().getTimeInMillis() / 1000);
+            int timeElapsed = timeNow - initTime;
+
+            //compare the time elapsed to the counter value
+            if (timeElapsed != counter) { //if they dont match we need to update the timeLeft and UI
+                Log.d("testTime", "timeElapsed: " + timeElapsed);
+                timeLeft--;
+                counter++; //the counter actually makes this impossible to cheat with calender change
+                publishProgress(timeLeft);
+            } else {
+                try {      //else wait 50ms and check again - this ensures 1/20th second accuracy
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            timeLeft--;
+
         }
 
         return null;
@@ -41,6 +63,10 @@ public class Async_Timer extends AsyncTask<Void, Integer, Void> {
     @Override
     protected void onPostExecute(Void aVoid) {
         triviaTimer.finishTimer();
+    }
+
+    @Override
+    protected void onCancelled() {
     }
 
     @Override
